140901b962324fe574ec182f7254ff2a9b48a190
MichaelPisciotta <mfp56@miami.edu>
2021-06-19 18:41:50 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution in this file!
+const employee = { name: 'mike', streetAddress:'60 warren'};
+
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+
+    newObj[key] = value;
+
+    return newObj;
+}
+updateEmployeeWithKeyAndValue(employee, 'name', 'Sam');
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value; 
+
+    return obj;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 broadway');
+
+
+
+
+
+function deleteFromEmployeeByKey(obj, key, value) {
+    const newObj = { ...obj };
+
+    newObj[key] = value;
+
+    return newObj;
+}   
+deleteFromEmployeeByKey(employee, 'name', '');
+
+
+
+
+
+
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+    obj[key] = value; 
+
+    return obj;
+}
+destructivelyDeleteFromEmployeeByKey(employee, 'streetAddress', '');
